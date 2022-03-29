57d6bb2aeb26040ed4eb62162eaed8f2e46f4b33
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-03-29 20:49:14 +0200

    Add serviceprovider

---
+++ src/SpecGeneratorServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\JsonapiOpenapiSpecGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class SpecGeneratorServiceProvider extends ServiceProvider
+{
+
+}
