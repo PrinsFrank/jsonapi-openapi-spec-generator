c09b7387ec46157a765f6d1e220745c8fb73f85e
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-03-31 20:54:43 +0200

    Build external docs

---
+++ src/Attributes/Server/OpenApiExternalDocs.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\JsonapiOpenapiSpecGenerator\Attributes\Server;
+
+use Attribute;
+use PrinsFrank\JsonapiOpenapiSpecGenerator\Attributes\OpenApiAttribute;
+
+#[Attribute]
+class OpenApiExternalDocs implements OpenApiAttribute
+{
+    public function __construct(public ?string $url = null, public ?string $description = null) { }
+}
--- src/Builders/ExternalDocs/ExternalDocsBuilder.php
+++ src/Builders/ExternalDocs/ExternalDocsBuilder.php
@@ -5,11 +5,24 @@ namespace PrinsFrank\JsonapiOpenapiSpecGenerator\Builders\ExternalDocs;
 
 use GoldSpecDigital\ObjectOrientedOAS\Objects\ExternalDocs;
 use LaravelJsonApi\Core\Server\Server;
+use PrinsFrank\JsonapiOpenapiSpecGenerator\Attributes\Server\OpenApiExternalDocs;
+use ReflectionClass;
 
 class ExternalDocsBuilder
 {
     public function build(Server $server): ?ExternalDocs
     {
+        foreach ((new ReflectionClass($server))->getAttributes() as $reflectionAttribute) {
+            $attribute = $reflectionAttribute->newInstance();
+            if ($attribute instanceof OpenApiExternalDocs === false || ($attribute->url === null && $attribute->description === null)) {
+                continue;
+            }
+
+            return (new ExternalDocs())
+                ->url($attribute->url)
+                ->description($attribute->description);
+        }
+
         return null;
     }
 }
