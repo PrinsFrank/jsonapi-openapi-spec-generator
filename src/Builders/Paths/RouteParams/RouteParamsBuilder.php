31c38c0e65b16f33250f6708a84bc8f4c4dd0134
Andries Mooij <a.mooij@gmail.com>
2023-01-04 12:30:43 +0100

    Add filters to generated spec

--- src/Builders/Paths/RouteParams/RouteParamsBuilder.php
+++ src/Builders/Paths/RouteParams/RouteParamsBuilder.php
@@ -7,11 +7,13 @@ namespace PrinsFrank\JsonapiOpenapiSpecGenerator\Builders\Paths\RouteParams;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 use Illuminate\Routing\Route;
+use LaravelJsonApi\Core\Server\Server;
+use LaravelJsonApi\Eloquent\Filters\Concerns\IsSingular;
 
 class RouteParamsBuilder
 {
     /** @return Parameter[] */
-    public static function build(Route $route): array
+    public static function build(Server $server, Route $route): array
     {
         preg_match_all('/{[A-z0-9]+}/', $route->uri(), $routeParamNames, PREG_PATTERN_ORDER);
         $routeParams = [];
@@ -21,6 +23,16 @@ class RouteParamsBuilder
                 ->name(trim($routeParamName, '{}'))
                 ->schema((new Schema())->type('integer'))
                 ->required();
+        }
+
+        if (isset($route->defaults['resource_type']) && str_contains($route->getName(), 'index')) {
+            $schema = $server->schemas()->schemaFor($route->defaults['resource_type']);
+            foreach ($schema->filters() as $filter) {
+                $routeParams[] = (new Parameter())
+                    ->in('query')
+                    ->name('filter[' . $filter->key() . ']')
+                    ->schema($filter->isSingular() ? Schema::string() : (new Schema())->type('array'));
+            }
         }
 
         return $routeParams;
