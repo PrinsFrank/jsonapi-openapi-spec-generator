1318897a13bbffe4e685b1a6599fcfce56803b62
Andries Mooij <a.mooij@gmail.com>
2023-01-06 09:38:20 +0100

    Revert parameter call and and type to array

--- src/Builders/Paths/RouteParams/RouteParamsBuilder.php
+++ src/Builders/Paths/RouteParams/RouteParamsBuilder.php
@@ -6,6 +6,7 @@ namespace PrinsFrank\JsonapiOpenapiSpecGenerator\Builders\Paths\RouteParams;
 
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\AnyOf;
 use Illuminate\Routing\Route;
 use LaravelJsonApi\Core\Server\Server;
 use LaravelJsonApi\Laravel\Routing\Route as JsonApiRoute;
@@ -25,14 +26,16 @@ class RouteParamsBuilder
                 ->required();
         }
 
-	$resourceType = $route->parameter(JsonApiRoute::RESOURCE_TYPE);
+        $resourceType = $route->defaults['resource_type'] ?? null;
         if ($resourceType !== null && str_contains($route->getName(), 'index')) {
             $schema = $server->schemas()->schemaFor($resourceType);
+            $stringOrInteger = (new AnyOf())->schemas(Schema::string(), Schema::integer());
+
             foreach ($schema->filters() as $filter) {
                 $routeParams[] = (new Parameter())
                     ->in('query')
                     ->name('filter[' . $filter->key() . ']')
-                    ->schema($filter->isSingular() ? Schema::string() : Schema::array());
+                    ->schema($filter->isSingular() ? Schema::string() : Schema::array()->items($stringOrInteger));
             }
         }
 
